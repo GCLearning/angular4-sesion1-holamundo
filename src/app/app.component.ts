027b5efc374259482fb811362160d3e776a33e2e
Giancarlo A. Chiappe Aguilar <gchiappe@outlook.com.pe>
2017-05-25 17:55:54 -0500

    Cambios del Ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = '¡Hola Mundito!';
+
+  lista = [
+    'Elemento 1',
+    'Elemento 2',
+    'Elemento 3',
+    "Flautas",
+    "Tambor",
+    "Guitarra Electrica"
+  ]
 }
